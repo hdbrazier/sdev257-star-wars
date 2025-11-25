47521092ad5eafd06a969a23eea3db649bb5dba3
hdbrazier <104693569+hdbrazier@users.noreply.github.com>
2025-11-25 12:20:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
 
-// Register for native environments (Expo Go/dev client)
+
 registerRootComponent(App);
 
-// Also default-export for Snack/Web's loader
+
 export default App;
